ff512478d754ae51c4a80f20c0e7ded7096c5868
Henry Sztanski <henry@sztanski.com>
2019-03-11 19:19:29 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -6,8 +8,41 @@ class Song
     @@all
   end
 
+  def self.create
+    new_instance = self.new
+    new_instance.save
+    new_instance
+  end
+
+  def self.new_by_name(name)
+
+  end
+
+  def self.create_by_name(name)
+
+  end
+
+  def self.find_by_name(name)
+
+  end
+
+  def self.new_from_filename(file)
+
+  end
+
+  def self.create_by_filename(file)
+
+  end
+
+  def self.alphabetical
+
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
   def save
     self.class.all << self
   end
-
 end
